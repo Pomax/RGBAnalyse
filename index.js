07877d2fd1e5d8f18d9abdcc98bb80dade9040f2
Mike Kamermans <pomax@nihongoresources.com>
2014-07-22 14:39:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-/* this code is released under an MIT license (http://opensource.org/licenses/MIT) */
+/* 
+  This code is released as Public Domain code.
+  In jurisdictions that do not recognise the public domain,
+  this code is to be considered under an MIT
+  license (http://opensource.org/licenses/MIT)
+*/
 module.exports = require("./src/RGBanalyse");
